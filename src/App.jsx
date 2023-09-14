9783ca0c8f3b2f1f9894e9f155b0ca6796c13fbe
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-09-14 19:28:31 +0600

    Initial Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl text-center font-bold my-10">
+        Course Registration
+      </h1>
+    </div>
+  );
+}
+
+export default App;
